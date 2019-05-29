a6e16baec963e9ca0c3632123de50d24f19d400f
Sarah Tam <sarahtam718@gmail.com>
2019-05-29 08:50:24 -0500

    navbar looking good and moving on to create component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import TodosList from "./components/todos-list.js";
+import EditTodo from "./components/edit-todo.js";
+import CreateTodo from "./components/create-todo.js";
+import toast from "./components/toast.png";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <h2>MERN Stack Todo Toast App</h2>
+        <nav className="navbar navbar-expand navbar-light bg-light">
+          <a
+            className="navbar-brand"
+            href="https://github.com/sarahtam718?tab=repositories"
+            target="_blank"
+          >
+            <img
+              src={toast}
+              style={{ width: 30, marginRight: 10 }}
+              alt="sarahtam718-github"
+            />
+          </a>
+          <Link to="/" className="navbar-brand">
+            Todo Toast
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Tasks
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">
+                  Create Task
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact component={TodosList} />
+        {/* path that accepts parameter of id */}
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
       </div>
-      <Route path="/" exact component={TodosList} />
-      {/* path that accepts parameter of id */}
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} />
     </Router>
   );
 }
---
+++ src/components/create-todo.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    };
+  }
+
+  onChangeTodoDescription(e) {
+    this.setState({ todo_description: e.target.value });
+  }
+
+  onChangeTodoResponsible(e) {
+    this.setState({ todo_responsible: e.target.value });
+  }
+
+  onChangeTodoPriority(e) {
+    this.setState({ todo_priority: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Welcome to Create Todo Component</p>
+      </div>
+    );
+  }
+}
