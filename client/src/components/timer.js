6b4372ccbd0667f03687a436ab3c7cbb4ffe5e56
Sarah Tam <sarahtam718@gmail.com>
2019-06-06 21:39:20 -0500

    clear interval not working

--- client/src/components/timer.js
+++ client/src/components/timer.js
@@ -4,13 +4,27 @@ export default class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 25
     };
   }
 
-  componentDidMount() {
-    this.setState({ count: this.state.count + 1 });
-  }
+  componentDidMount = () => {
+    this.theInterval = setInterval(() => {
+      this.setState({ count: this.state.count - 1 });
+    }, 1000);
+
+    if (this.state.count === 0) {
+      clearInterval(this.theInterval);
+    }
+  };
+
+  //   refresh() {
+  //       this.setState({count: this.state.count})
+  //   }
+
+  componentWillUnmount = () => {
+    clearInterval(this.theInterval);
+  };
   render() {
     return <div>Timer: {this.state.count}</div>;
   }
