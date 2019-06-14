289507d9e13c63eb40d972c1b6fc6d9481328182
Sarah Tam <sarahtam718@gmail.com>
2019-06-14 08:59:20 -0500

    working on countdown timer

--- client/src/components/timer.js
+++ client/src/components/timer.js
@@ -8,24 +8,52 @@ export default class Timer extends Component {
     };
   }
 
-  componentDidMount = () => {
-    this.theInterval = setInterval(() => {
-      this.setState({ count: this.state.count - 1 });
-    }, 1000);
+  // componentDidMount = () => {
+  //   this.theInterval = setInterval(() => {
+  //     this.setState({ count: this.state.count - 1 });
+  //   }, 1000);
 
-    if (this.state.count === 0) {
-      clearInterval(this.theInterval);
-    }
-  };
+  //   if (this.state.count === 0) {
+  //     clearInterval(this.theInterval);
+  //   }
+  // };
 
   //   refresh() {
   //       this.setState({count: this.state.count})
   //   }
 
-  componentWillUnmount = () => {
-    clearInterval(this.theInterval);
-  };
+  // componentWillUnmount = () => {
+  //   clearInterval(this.theInterval);
+  // };
   render() {
-    return <div>Timer: {this.state.count}</div>;
+    // return <div>Timer: {this.state.count}</div>;
+    return (
+      <div class="timer">
+        <div class="timer__controls">
+          <button data-time="20" class="timer__button">
+            20 Secs
+          </button>
+          <button data-time="300" class="timer__button">
+            Work 5
+          </button>
+          <button data-time="900" class="timer__button">
+            Quick 15
+          </button>
+          <button data-time="1200" class="timer__button">
+            Snack 20
+          </button>
+          <button data-time="3600" class="timer__button">
+            Lunch Break
+          </button>
+          <form name="customForm" id="custom">
+            <input type="text" name="minutes" placeholder="Enter Minutes" />
+          </form>
+        </div>
+        <div class="display">
+          <h1 class="display__time-left" />
+          <p class="display__end-time" />
+        </div>
+      </div>
+    );
   }
 }
